9c7ca489c2daf7a4b54cc7e712fb9e8eac35a6dd
Mansoor Anis <mansooranis60@gmail.com>
2025-10-25 22:46:25 -0600

    Added BaseWalker

---
+++ include/ast/walkers/AstWalker.h
@@ -0,0 +1,125 @@
+#include "ast/Ast.h"
+#include "ast/RootAst.h"
+#include "ast/expressions/BoolAst.h"
+#include "ast/expressions/CastAst.h"
+#include "ast/expressions/CharAst.h"
+#include "ast/expressions/FuncProcCallAst.h"
+#include "ast/expressions/IdentifierAst.h"
+#include "ast/expressions/IntegerAst.h"
+#include "ast/expressions/RealAst.h"
+#include "ast/expressions/TupleAccessAst.h"
+#include "ast/expressions/UnaryAst.h"
+#include "ast/prototypes/FunctionAst.h"
+#include "ast/prototypes/FunctionParamAst.h"
+#include "ast/prototypes/ProcedureAst.h"
+#include "ast/prototypes/ProcedureParamAst.h"
+#include "ast/statements/AssignmentAst.h"
+#include "ast/statements/BlockAst.h"
+#include "ast/statements/BreakAst.h"
+#include "ast/statements/ConditionalAst.h"
+#include "ast/statements/ContinueAst.h"
+#include "ast/statements/DeclarationAst.h"
+#include "ast/statements/IdentifierLeftAst.h"
+#include "ast/statements/InputAst.h"
+#include "ast/statements/OutputAst.h"
+#include "ast/statements/ReturnAst.h"
+#include "ast/statements/TupleAssignAst.h"
+#include "ast/statements/TypealiasAst.h"
+
+#include <any>
+namespace gazprea::ast::walkers {
+  class AstWalker {
+  public:
+    AstWalker();
+    ~AstWalker() = default;
+
+    std::any visit(std::shared_ptr<Ast> ast);
+    virtual std::any visitRoot(std::shared_ptr<RootAst> ctx) {
+      return {};
+    }
+    virtual std::any visitAssignment(std::shared_ptr<statements::AssignmentAst> ctx) {
+      return {};
+    }
+    virtual std::any visitDeclaration(std::shared_ptr<statements::DeclarationAst> ctx) {
+      return {};
+    }
+    virtual std::any visitBlock(std::shared_ptr<statements::BlockAst> ctx) {
+      return {};
+    }
+    virtual std::any visitBreak(std::shared_ptr<statements::BreakAst> ctx) {
+      return {};
+    }
+    virtual std::any visitContinue(std::shared_ptr<statements::ContinueAst> ctx) {
+      return {};
+    }
+    virtual std::any visitConditional(std::shared_ptr<statements::ConditionalAst> ctx) {
+      return {};
+    }
+    virtual std::any visitInput(std::shared_ptr<statements::InputAst> ctx) {
+      return {};
+    }
+    virtual std::any visitOutput(std::shared_ptr<statements::OutputAst> ctx) {
+      return {};
+    }
+    virtual std::any visitProcedure(std::shared_ptr<prototypes::ProcedureAst> ctx) {
+      return {};
+    }
+    virtual std::any visitProcedureParams(std::shared_ptr<prototypes::ProcedureParamAst> ctx) {
+      return {};
+    }
+    virtual std::any visitProcedureCall(std::shared_ptr<statements::ProcedureCallAst> ctx) {
+      return {};
+    }
+    virtual std::any visitReturn(std::shared_ptr<statements::ReturnAst> ctx) {
+      return {};
+    }
+    virtual std::any visitTupleAssign(std::shared_ptr<statements::TupleAssignAst> ctx) {
+      return {};
+    }
+    virtual std::any visitTupleAccess(std::shared_ptr<expressions::TupleAccessAst> ctx) {
+      return {};
+    }
+    virtual std::any visitTypealias(std::shared_ptr<statements::TypealiasAst> ctx) {
+      return {};
+    }
+    virtual std::any visitFunction(std::shared_ptr<prototypes::FunctionAst> ctx) {
+      return {};
+    }
+    virtual std::any visitFunctionParam(std::shared_ptr<prototypes::FunctionParamAst> ctx) {
+      return {};
+    }
+    virtual std::any visitPrototype(std::shared_ptr<prototypes::PrototypeAst> ctx) {
+      return {};
+    }
+    virtual std::any visitFuncProcCall(std::shared_ptr<expressions::FuncProcCallAst> ctx) {
+      return {};
+    }
+    virtual std::any visitArg(std::shared_ptr<expressions::ArgAst> ctx) {
+      return {};
+    }
+    virtual std::any visitBool(std::shared_ptr<expressions::BoolAst> ctx) {
+      return {};
+    }
+    virtual std::any visitCast(std::shared_ptr<expressions::CastAst> ctx) {
+      return {};
+    }
+    virtual std::any visitChar(std::shared_ptr<expressions::CharAst> ctx) {
+      return {};
+    }
+    virtual std::any visitIdentifier(std::shared_ptr<expressions::IdentifierAst> ctx) {
+      return {};
+    }
+    virtual std::any visitIdentifierLeft(std::shared_ptr<statements::IdentifierLeftAst> ctx) {
+      return {};
+    }
+    virtual std::any visitInteger(std::shared_ptr<expressions::IntegerAst> ctx) {
+      return {};
+    }
+    virtual std::any visitReal(std::shared_ptr<expressions::RealAst> ctx) {
+      return {};
+    }
+    virtual std::any visitUnary(std::shared_ptr<expressions::UnaryAst> ctx) {
+      return {};
+    }
+  };
+}
---
+++ src/ast/walkers/AstWalker.cpp
@@ -0,0 +1,69 @@
+#include "ast/statements/ProcedureCallAst.h"
+
+#include <ast/walkers/AstWalker.h>
+
+namespace gazprea::ast::walkers {
+
+std::any AstWalker::visit(std::shared_ptr<Ast> ast) {
+  switch (ast->getNodeType()) {
+  case NodeType::Arg:
+    return visitArg(std::static_pointer_cast<expressions::ArgAst>(ast));
+  case NodeType::Assignment:
+    return visitAssignment(std::static_pointer_cast<statements::AssignmentAst>(ast));
+  case NodeType::Block:
+    return visitBlock(std::static_pointer_cast<statements::BlockAst>(ast));
+  case NodeType::Break:
+    return visitBreak(std::static_pointer_cast<statements::BreakAst>(ast));
+  case NodeType::Bool:
+    return visitBool(std::static_pointer_cast<expressions::BoolAst>(ast));
+  case NodeType::Continue:
+    return visitContinue(std::static_pointer_cast<statements::ContinueAst>(ast));
+  case NodeType::Conditional:
+    return visitConditional(std::static_pointer_cast<statements::ConditionalAst>(ast));
+  case NodeType::Cast:
+    return visitCast(std::static_pointer_cast<expressions::CastAst>(ast));
+  case NodeType::Char:
+    return visitChar(std::static_pointer_cast<expressions::CharAst>(ast));
+  case NodeType::Declaration:
+    return visitDeclaration(std::static_pointer_cast<statements::DeclarationAst>(ast));
+  case NodeType::Function:
+    return visitFunction(std::static_pointer_cast<prototypes::FunctionAst>(ast));
+  case NodeType::FunctionParam:
+    return visitFunctionParam(std::static_pointer_cast<prototypes::FunctionParamAst>(ast));
+  case NodeType::FuncProcCall:
+    return visitFuncProcCall(std::static_pointer_cast<expressions::FuncProcCallAst>(ast));
+  case NodeType::Procedure:
+    return visitProcedure(std::static_pointer_cast<prototypes::ProcedureAst>(ast));
+  case NodeType::ProcedureParam:
+    return visitProcedureParams(std::static_pointer_cast<prototypes::ProcedureParamAst>(ast));
+  case NodeType::ProcedureCall:
+    return visitProcedureCall(std::static_pointer_cast<statements::ProcedureCallAst>(ast));
+  case NodeType::Prototype:
+    return visitPrototype(std::static_pointer_cast<prototypes::PrototypeAst>(ast));
+  case NodeType::Output:
+    return visitOutput(std::static_pointer_cast<statements::OutputAst>(ast));
+  case NodeType::Identifier:
+    return visitIdentifier(std::static_pointer_cast<expressions::IdentifierAst>(ast));
+  case NodeType::IdentifierLeft:
+    return visitIdentifierLeft(std::static_pointer_cast<statements::IdentifierLeftAst>(ast));
+  case NodeType::Integer:
+    return visitInteger(std::static_pointer_cast<expressions::IntegerAst>(ast));
+  case NodeType::Input:
+    return visitInput(std::static_pointer_cast<statements::InputAst>(ast));
+  case NodeType::Real:
+    return visitReal(std::static_pointer_cast<expressions::RealAst>(ast));
+  case NodeType::Return:
+    return visitReturn(std::static_pointer_cast<statements::ReturnAst>(ast));
+  case NodeType::Root:
+    return visitRoot(std::static_pointer_cast<RootAst>(ast));
+  case NodeType::TupleAccess:
+    return visitTupleAccess(std::static_pointer_cast<expressions::TupleAccessAst>(ast));
+  case NodeType::Typealias:
+    return visitTypealias(std::static_pointer_cast<statements::TypealiasAst>(ast));
+  case NodeType::TupleAssign:
+    return visitTupleAssign(std::static_pointer_cast<statements::TupleAssignAst>(ast));
+  case NodeType::UnaryExpression:
+    return visitUnary(std::static_pointer_cast<expressions::UnaryAst>(ast));
+  }
+}
+} // namespace gazprea::ast::walkers
