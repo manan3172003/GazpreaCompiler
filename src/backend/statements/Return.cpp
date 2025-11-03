0921d8b761036655007d8eac3f2aa7b078cd6512
Armaan Katyal <armaankatyal3@gmail.com>
2025-11-03 16:29:13 -0700

    support all types from return and promotion

--- src/backend/statements/Return.cpp
+++ src/backend/statements/Return.cpp
@@ -1,24 +1,37 @@
+#include "ast/walkers/ValidationWalker.h"
 #include "backend/Backend.h"
+#include "symTable/MethodSymbol.h"
+#include "symTable/TupleTypeSymbol.h"
 
 namespace gazprea::backend {
 
 std::any Backend::visitReturn(std::shared_ptr<ast::statements::ReturnAst> ctx) {
-  if (ctx->getExpr()) {
-    visit(ctx->getExpr());
-
-    auto [type, value] = ctx->getScope()->getTopElementInStack();
-    ctx->getScope()->popElementFromScopeStack();
-
-    if (ctx->getExpr()->getNodeType() == ast::NodeType::IntegerLiteral) {
-      auto loadOp = builder->create<mlir::LLVM::LoadOp>(loc, intTy(), value);
-      builder->create<mlir::LLVM::ReturnOp>(builder->getUnknownLoc(), loadOp.getResult());
-      return {};
-    }
-    // TODO: Handle other return types
-  } else {
+  if (!ctx->getExpr()) {
     builder->create<mlir::LLVM::ReturnOp>(builder->getUnknownLoc(), mlir::ValueRange{});
+    return {};
   }
 
+  visit(ctx->getExpr());
+  auto [type, value] = ctx->getScope()->getTopElementInStack();
+  ctx->getScope()->popElementFromScopeStack();
+
+  const auto methodScope = std::dynamic_pointer_cast<symTable::MethodSymbol>(
+      ast::walkers::ValidationWalker::getEnclosingFuncProcScope(ctx->getScope()));
+
+  if (type->getName() == "tuple") {
+    // Promote the tuple value to the return value
+    castIfNeeded(value, type, methodScope->getReturnType());
+    auto loadOp =
+        builder->create<mlir::LLVM::LoadOp>(loc, getMLIRType(methodScope->getReturnType()), value);
+    builder->create<mlir::LLVM::ReturnOp>(builder->getUnknownLoc(), loadOp.getResult());
+  } else {
+    // handle all scalar types
+    castIfNeeded(value, type, methodScope->getReturnType());
+    auto loadOp =
+        builder->create<mlir::LLVM::LoadOp>(loc, getMLIRType(methodScope->getReturnType()), value);
+    builder->create<mlir::LLVM::ReturnOp>(builder->getUnknownLoc(), loadOp.getResult());
+  }
+  // TODO: Handle other return types
   return {};
 }
 } // namespace gazprea::backend
