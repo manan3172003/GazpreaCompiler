1c67038b1006fb1186769f9ab270678455651ad6
Armaan Katyal <armaankatyal3@gmail.com>
2025-11-02 19:26:06 -0700

    support func fwd decl mlir

--- src/backend/prototypes/Function.cpp
+++ src/backend/prototypes/Function.cpp
@@ -7,21 +7,30 @@ std::any Backend::visitFunction(std::shared_ptr<ast::prototypes::FunctionAst> ct
   const auto methodSym =
       std::dynamic_pointer_cast<symTable::MethodSymbol>(ctx->getProto()->getSymbol());
   const auto savedInsertPoint = builder->saveInsertionPoint();
+  const bool isForwardDecl = !ctx->getBody();
+
   const auto funcReturnType = getMLIRType(methodSym->getReturnType());
   auto funcType = mlir::LLVM::LLVMFunctionType::get(
       funcReturnType, getMethodParamTypes(ctx->getProto()->getParams()), false);
-  auto funcOp = builder->create<mlir::LLVM::LLVMFuncOp>(loc, methodSym->getName(), funcType);
-  mlir::Block *entry = funcOp.addEntryBlock();
-  builder->setInsertionPointToStart(entry);
 
-  blockArg.clear();
-  size_t argIndex = 0;
-  for (const auto &param : ctx->getProto()->getParams()) {
-    const auto paramNode = std::dynamic_pointer_cast<ast::prototypes::ProcedureParamAst>(param);
-    blockArg[paramNode->getName()] = entry->getArgument(argIndex++);
+  auto funcOp = module.lookupSymbol<mlir::LLVM::LLVMFuncOp>(methodSym->getName());
+  if (!funcOp) {
+    funcOp = builder->create<mlir::LLVM::LLVMFuncOp>(loc, methodSym->getName(), funcType);
   }
-  visit(ctx->getProto());
-  visit(ctx->getBody());
+
+  if (!isForwardDecl) {
+    mlir::Block *entry = funcOp.addEntryBlock();
+    builder->setInsertionPointToStart(entry);
+
+    blockArg.clear();
+    size_t argIndex = 0;
+    for (const auto &param : ctx->getProto()->getParams()) {
+      const auto paramNode = std::dynamic_pointer_cast<ast::prototypes::ProcedureParamAst>(param);
+      blockArg[paramNode->getName()] = entry->getArgument(argIndex++);
+    }
+    visit(ctx->getProto());
+    visit(ctx->getBody());
+  }
   builder->restoreInsertionPoint(savedInsertPoint);
   return {};
 }
