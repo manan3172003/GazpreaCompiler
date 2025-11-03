65e189ad99faff9fd740964c8a493dd8a82361d8
Armaan Katyal <armaankatyal3@gmail.com>
2025-11-02 20:20:29 -0700

    support nested loops

--- src/backend/statements/Block.cpp
+++ src/backend/statements/Block.cpp
@@ -5,6 +5,13 @@ namespace gazprea::backend {
 std::any Backend::visitBlock(std::shared_ptr<ast::statements::BlockAst> ctx) {
   for (const auto &child : ctx->getChildren()) {
     visit(child);
+
+    // Stop visiting further statements if a terminator was created
+    if (!builder->getBlock()->empty() &&
+        (mlir::isa<mlir::scf::YieldOp>(builder->getBlock()->back()) ||
+         mlir::isa<mlir::LLVM::ReturnOp>(builder->getBlock()->back()))) {
+      break;
+    }
   }
   ctx->getScope()->getScopeStack().clear();
   return {};
