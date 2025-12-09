9e26cbdd094f67aab0ec06083ad2ca72bb0c6af2
Manan Jain <mananj146@gmail.com>
2025-12-08 17:24:40 -0700

    concat follows other convention

--- src/backend/prototypes/MemberFunc.cpp
+++ src/backend/prototypes/MemberFunc.cpp
@@ -355,11 +355,13 @@ std::any Backend::visitPushMemberFunc(std::shared_ptr<ast::statements::PushMembe
 std::any Backend::visitConcatMemberFunc(std::shared_ptr<ast::statements::ConcatMemberFuncAst> ctx) {
   visit(ctx->getLeft());
 
-  auto [vectorType, vectorAddr] = popElementFromStack(ctx);
-  if (auto varSym =
-          std::dynamic_pointer_cast<symTable::VariableSymbol>(ctx->getLeft()->getSymbol())) {
-    vectorType = varSym->getType();
-    vectorAddr = varSym->value;
+  auto [vectorType, vectorAddr] = popElementFromStack(ctx->getLeft());
+  if (!vectorType || !vectorAddr) {
+    if (auto varSym =
+            std::dynamic_pointer_cast<symTable::VariableSymbol>(ctx->getLeft()->getSymbol())) {
+      vectorType = varSym->getType();
+      vectorAddr = varSym->value;
+    }
   }
   auto vectorTypeSym = std::dynamic_pointer_cast<symTable::VectorTypeSymbol>(vectorType);
   if (!vectorTypeSym) {
